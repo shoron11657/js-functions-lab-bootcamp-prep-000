3be536e3850aad89f973d57c8c23796322a87480
Kevin M <shoron@gmail.com>
2017-11-13 07:25:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function happyHolidays(){return ('Happy holidays!')}
+<<<<<<< HEAD
 function happyHolidaysTo(name){return(`Happy holidays, ${name}!`)}
 function happyHolidayTo(holiday, name){return(`Happy ${holiday}, ${name}!`)}
 function holidayCountdown(holiday, days)
   {return (`It's ${days} days until ${holiday}!`)}
+=======
+function happyHolidaysTo (name) {return ('Happy holidays,${name}!')}
+>>>>>>> 0600ef65bc18c7002b191dfef53b7e7b75d95c09
